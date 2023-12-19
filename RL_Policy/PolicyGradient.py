131cd58ee47d7bbf02bff1843291e4a13fb77122
Archy Zheng Chengqi <zhen0144@e.ntu.edu.sg>
2023-12-19 09:21:25 +0800

    Finish the policy gradient algorithm

--- RL_Policy/PolicyGradient.py
+++ RL_Policy/PolicyGradient.py
@@ -2,8 +2,8 @@ from collections import namedtuple
 import torch
 from torch.distributions import Distribution, Normal, Categorical
 import torch.nn as nn
-
-transition = namedtuple('Transition', ('state', 'action', 'next_state', 'reward'))
+import numpy as np
+from torch.utils.tensorboard import SummaryWriter
 
 
 class PolicyNetwork(nn.Module):
@@ -29,16 +29,45 @@ class PolicyNetwork(nn.Module):
         state = torch.from_numpy(state).float()
 
         if self.action_type == 'discrete':
-            return Categorical(self.model(state))
+            return Categorical(logits=self.model(state))
         else:
             return Normal(self.model(state), torch.exp(self.log_std))
 
 
 class PolicyGradient:
-    def __init__(self, env):
-        action_dim = env.action_space.shape[0]
+    def __init__(self, env, memory):
+        action_dim = env.action_space.n
         observation_dim = env.observation_space.shape[0]
 
+        from torch.optim import Adam
+        import itertools
 
+        self.memory = memory
+        LR = 1e-3
+        self.BATCH_SIZE = 100
+        self.policy_network = PolicyNetwork(observation_dim, action_dim)
+        if self.policy_network.action_type == 'continuous':
+            self.optimizer = Adam(
+                params=(itertools.chain([self.policy_network.log_std], self.policy_network.parameters())), lr=LR)
+        else:
+            self.optimizer = Adam(params=self.policy_network.parameters(), lr=LR)
+        self.summary = SummaryWriter()
+        self.global_step = 0
 
+    def train(self):
+        loss = 0
+        number_action = 0
+        for i in range(self.BATCH_SIZE):
+            state_batch = np.array(self.memory[i]['observation'])
+            action_batch = torch.as_tensor(np.array(self.memory[i]['action']))
+            number_action += action_batch.shape[0]
+            loss = -1 * self.policy_network(state_batch).log_prob(action_batch).sum() * self.memory[i]['return'] + loss
+        loss = loss / number_action
+        self.summary.add_scalar("loss", loss, self.global_step)
+        self.global_step += 1
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
 
+    def __call__(self, state):
+        return self.policy_network(state)
--- Task/Ant_PolicyGradient.py
+++ Task/Ant_PolicyGradient.py
@@ -1,9 +1,35 @@
 import gymnasium as gym
 import numpy as np
+from RL_Policy.PolicyGradient import PolicyGradient
 
 
-def main(**kwargs):
-    env = gym.make('Ant')
+def main():
+    env = gym.make('CartPole-v1', render_mode='human')
+    BATCH_SIZE = 100
+    current_times_of_task = 0
+    memory = [{} for _ in range(BATCH_SIZE)]
+    policy_gradient = PolicyGradient(env, memory)
+    for _ in range(10000):
+        observation, info = env.reset()
+        ob_batch = []
+        action_batch = []
+        rewards_batch = []
+        while True:
+            action = policy_gradient(observation).sample().detach().numpy()
+            action_batch.append(action)
+            next_observation, reward, done, truncated, info = env.step(action)
+            rewards_batch.append(reward)
+            ob_batch.append(observation)
+            observation = next_observation
+            if done or truncated:
+                current_index = current_times_of_task % BATCH_SIZE
+                memory[current_index]['observation'] = ob_batch
+                memory[current_index]['action'] = action_batch
+                memory[current_index]['return'] = sum(rewards_batch)
+                current_times_of_task += 1
+                if current_times_of_task % BATCH_SIZE == 0:
+                    policy_gradient.train()
+                break
 
 
 if __name__ == '__main__':
